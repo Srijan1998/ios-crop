eabf99f1d88b1e194dd9ce80ea51e87e32c3177a
Srijan1998 <srijanbhatiasun@gmail.com>
2019-05-27 21:58:10 +0530

    Add files via upload

--- crop/crop/ViewController.m
+++ crop/crop/ViewController.m
@@ -16,29 +16,20 @@
 
 @implementation ViewController
 
-/*Overrding inbuilt methods to draw the bezier curve*/
-- (void) drawRect:(CGPoint)Rect
-{
-    //this method is not used
-    [self.path fill];
-    //[self.path stroke];
-}
-//Use CAShapeLayer to redraw rthe view
+
+//Use CAShapeLayer to redraw the view
 - (void) newMethod
 {
     UIBezierPath *shape = self.path;
     CAShapeLayer *shapeLayer = [CAShapeLayer layer];
     shapeLayer.path = shape.CGPath;
-//    shapeLayer.fillRule = kCAFillRuleEvenOdd;
     shapeLayer.fillColor = [UIColor clearColor].CGColor;
-//    shapeLayer.opacity=1;
-//    shapeLayer.backgroundColor = [UIColor clearColor].CGColor;
     shapeLayer.strokeColor = [UIColor blackColor].CGColor;
     shapeLayer.lineWidth = 2;
-//    [self.view.layer setMask:shapeLayer];
     [self.view.layer addSublayer:shapeLayer];
     
 }
+/*Overrding inbuilt methods to draw the bezier curve*/
 - (void) touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
     self.path = [[UIBezierPath alloc] init];
@@ -56,9 +47,7 @@
     touch = [touches allObjects];
     UITouch *touch1 = touch[0];
     CGPoint middle = [touch1 preciseLocationInView:touch1.view];
-    //[self.path moveToPoint:middle];
     [self.path addLineToPoint:middle];
-    //[self.view setNeedsDisplay];
     [self newMethod];
 }
 - (void) touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
@@ -67,15 +56,10 @@
     touch = [touches allObjects];
     UITouch *touch1 = touch[0];
     CGPoint end = [touch1 preciseLocationInView:touch1.view];
-    //[self.path moveToPoint:end];
     [self.path addLineToPoint:end];
     [self.path closePath];
     [self newMethod];
-    //[self.view setNeedsDisplay];
-    
-    
-    
-//    self.InputImage.image = image;
+
 }
 - (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
@@ -93,9 +77,6 @@
 - (IBAction)CropClicked:(id)sender {
     CAShapeLayer *shapeLayer = [CAShapeLayer layer];
     shapeLayer.path = self.path.CGPath;
-    //    shapeLayer.fillColor = [UIColor clearColor].CGColor;
-    shapeLayer.backgroundColor = [UIColor whiteColor].CGColor;
-    self.view.backgroundColor = [UIColor whiteColor];
     [self.view.layer setMask:shapeLayer];
     UIGraphicsBeginImageContext(self.view.bounds.size);
     [self.view.layer renderInContext:UIGraphicsGetCurrentContext()];
@@ -104,7 +85,16 @@
     
     [self performSegueWithIdentifier:@"Click" sender:nil];
 }
+- (IBAction)CancelClicked:(id)sender {
+    [self.path removeAllPoints];
+    
+    for (CALayer *layer in [self.view.layer.sublayers copy])
+    {
+        if([layer isKindOfClass:[CAShapeLayer class]])
+        {
+            [layer removeFromSuperlayer];
+        }
+    }
+}
 
-/*- (IBAction)CropClicked:(id)sender {
-}*/
 @end
